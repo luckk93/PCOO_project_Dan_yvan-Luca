9e778330c036280e545ae0a42af9669f97633567
0xff800000 <0xff800000@gmail.com>
2017-12-12 17:41:31 +0100

    merge

--- PCOO-Projet/plot.sh
+++ PCOO-Projet/plot.sh
@@ -1,2 +1,2 @@
-plot "waveforms.dat" using 1 with lines title 'ValPhen', "waveforms.dat" using 2 with lines title 'ValEtat', "waveforms.dat" using 3 with lines title 'ValCtrl'
+plot "waveforms.dat" using 2 with lines title 'ValPhen', "waveforms.dat" using 3 with lines title 'ValEtat', "waveforms.dat" using 4 with lines title 'ValCtrl'
 pause -1
